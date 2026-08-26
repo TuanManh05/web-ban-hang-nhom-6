337723fb872a2dfc0f02151eece47210d6d419a0
Khoapham123456 <khoapd1917@ut.edu.vn>
2026-08-26 23:07:55 +0700

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/../models/Product.php';
+
 final class HomeController
 {
     public function index(): void
     {
         $pageTitle = 'Trang chủ';
+        $products = Product::featured();
+
         require __DIR__ . '/../views/home.php';
     }
 }
