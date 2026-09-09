3ec28cd5461813fd40edfa1a065199ec782e46f9
Pham Tuan Manh <phamtuanmanh141021@gmail.com>
2026-09-09 16:15:55 +0700

    fix: hien thi dung anh san pham tren trang chu

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,15 +2,13 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../models/Product.php';
 
 final class HomeController
 {
     public function index(): void
     {
-        $conn = database();
-        $stmt = $conn->query("SELECT * FROM products ORDER BY id DESC");
-        $products = $stmt->fetchAll();
-        $basePath = ''; 
+        $products = Product::featured(8);
 
         require __DIR__ . '/../views/home.php';
     }
